28ced7183e3fe2e74a7db8db93a15c56b0a1a2c3
facucarattoliUCC <2418100@ucc.edu.ar>
2025-09-08 11:45:01 -0300

    empezamos con JS

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,45 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function convertUnidades(unidad, valor){
+    let metro, pie, pulgada, yarda //defino variables
+
+    console.log(valor); //saber input usuario
+    console.log(isNaN(valor));
+
+    if(isNaN(valor)){
+        alert("Valor ingresado incorrecto");
+        metro = "";
+        pie = "";
+        yarda = "";
+        pulgada = "";
+    } else {
+        if (unidad == "unid_metro") {
+            metro = valor;
+            pie = 3.28 * metro;
+            pulgada = 39.37 * metro;
+            yarda = 1.0936 * metro;
+        } else if (unidad == "unid_pie") {
+            pie = valor;
+            metro = 0.3048 * pie;
+            pulgada = 12 * pie;
+            yarda = 0.3333 * pie;
+        } else if (unidad == "unid_pulgada") {
+            pulgada = valor;
+            metro = 0.0254 * pulgada;
+            pie = 0.0833 * pulgada;
+            yarda = 0.02777 * pulgada;
+        } else if (unidad == "unid_yarda") {
+            yarda = valor;
+            metro = 0.9144 * yarda;
+            pie = 3 * yarda;
+            pulgada = 36 * yarda;
+        }
+    }
+    document.getElementById("metro").value = metro;
+    document.getElementById("pie").value = pie;
+    document.getElementById("pulgada").value = pulgada;
+    document.getElementById("yarda").value = yarda;
+
+}
